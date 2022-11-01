31d96fa4990056fffc12a6ed4d8bf409ffa0940c
Sancondem <santiago.conde@ucp.edu.co>
2022-11-01 16:30:44 -0500

    First commit

---
+++ src/pages/FirstPage.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+export const FirstPage = () => {
+    const [valor, setValor] = useState(10)
+
+    const modificarEstado = (num) => {
+
+        
+            setValor(Math.max(valor + num, 0))
+    }
+
+    const resetValor = () => {
+
+        
+        setValor(10)
+}
+
+    return (
+
+        <>
+            <h1>Contador</h1>
+            <h1>{valor}</h1>
+            <button onClick={() => modificarEstado(+1)}>+1</button>
+            <button onClick={() => modificarEstado(-1)}>-1</button>
+            <button onClick={resetValor}>Reset</button>
+        </>
+    )
+}
